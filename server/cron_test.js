36a1bc901d5b95b265dd48da80c8eaa803a2547b
Riza Fahmi <rizafahmi@gmail.com>
2015-01-08 15:11:01 +0700

    Test sending email at 16.00 WIB

--- server/cron_test.js
+++ server/cron_test.js
@@ -3,15 +3,17 @@ var sendEmail = function () {
   body += "<h2>And what is your grateful for today?</h2>";
   body += "<p>Take 1 minute to write out what you got done today and what is your grateful for today. Click <a href='http://idttg.com'>this link</a> and start write it out all your done tasks and what your grateful for.</p>";
 
-  Meteor.call('emailFeedback', 'rizafahmi+idttg@gmail.com', body, {}, function (error, data) {
-    if (error)
-      console.log(error);
-    console.log(data);
+  Meteor.users.find({}).forEach(function (user) {
+    Meteor.call('emailFeedback', user.emails[0].address, body, {}, function (error, data) {
+      if (error)
+        console.log(error);
+      console.log(user.emails[0].address);
+    });
   });
 }
 
 var cron = new Meteor.Cron({
     events: {
-      "* * * * *": sendEmail
+      "0 16 * * *": sendEmail
     }
 });
