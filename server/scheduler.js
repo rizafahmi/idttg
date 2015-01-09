05880c9d17c5f4b6bf935977b6e42e8e55f61ace
Riza Fahmi <rizafahmi@gmail.com>
2015-01-09 10:17:29 +0700

    Sending email to verified user only

--- server/scheduler.js
+++ server/scheduler.js
@@ -3,16 +3,12 @@ var sendEmail = function () {
   body += "<h2>And what is your grateful for today?</h2>";
   body += "<p>Take 1 minute to write out what you got done today and what is your grateful for today. Click <a href='http://idttg.com'>this link</a> and start write it out all your done tasks and what your grateful for.</p>";
 
-  Meteor.users.find({}).forEach(function (user) {
-    if (user.emails[0].verified === true) {
-      Meteor.call('emailFeedback', user.emails[0].address, body, {}, function (error, data) {
-        if (error)
-          console.log(error);
-        console.log(user.emails[0].address);
-      });
-    } else {
-      console.log("Email ", user.emails[0].address, " not verified yet. Not sending reminder to them");
-    }
+  Meteor.users.find({"emails.verified": true}).forEach(function (user) {
+    Meteor.call('emailFeedback', user.emails[0].address, body, {}, function (error, data) {
+      if (error)
+        console.log(error);
+      console.log(user.emails[0].address);
+    });
   });
 }
 
