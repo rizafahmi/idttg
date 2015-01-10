518f8ed246e9a68d265c0b170e8ae575b2146c59
Riza Fahmi <rizafahmi@gmail.com>
2015-01-10 20:10:38 +0700

    Daily dones finish show calendar. Next: show data. WIP

---
+++ client/controllers/dones/dones_daily.js
@@ -0,0 +1,25 @@
+DonesDailyController = RouteController.extend({
+  waitOn: function () {
+    var date = this.params.date;
+    var month = this.params.month;
+    var year = this.params.year;
+    var startDateObject = moment(year + "-" + month + "-" + date);
+    var endDateObject = moment(year + "-" + month + "-" + date);
+    var startDate = startDateObject.startOf('day');
+    var endDate = endDateObject.endOf('day');
+    Session.set("date", year + "-" + month + "-" + date);
+
+    return [
+      Meteor.subscribe('dones_index', Meteor.userId())
+    ];
+
+  },
+
+  data: function () {
+
+  },
+
+  action: function () {
+    this.render();
+  }
+});
